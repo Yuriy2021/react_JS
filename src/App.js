173ef3de87ca00ce05682e0a0824f0503abe0208
Yuriy <xstream_06@mail.ru>
2022-01-20 01:08:17 +0300

    Lesson2HW

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Message } from './components';
+
+import { Message, MessageList } from './components';
 
 function App() {
   return (
     <div className="App">
       <Message text={'Text through props update'}></Message>
-      <Message color="alert" text={'Text through props update alternate'}></Message>
-      <Message color="primary" text={'Text through props update exclusive'}></Message>
+      <Message color="primary" text={'Text through props update alternate'}></Message>
+      <Message color="alert" text={'Text through props update exclusive'}></Message>
+      <MessageList />
     </div>
+
   );
 }
 
