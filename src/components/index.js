d59b132ed4e62ca14905e2ac8d049ee51c0cc94d
Yuriy <xstream_06@mail.ru>
2022-01-24 20:14:35 +0300

    Lesson3HW

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-import  { useState, useEffect } from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import {ChatList} from "/components/Chatlist";
-import {MessageInput} from "./component/MessageInput";
-import {MessageList} from "./components/MessageList";
+import { useState, useEffect } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { ChatList } from "./components/Chatlist";
+import { MessageInput } from "./component/MessageInput";
+import { MessageList } from "./components/MessageList";
 
 const useStyles = makeStyles({
     wrapper: {
@@ -15,62 +15,61 @@ export const Format = () => {
     const classes = useStyles();
     const [messageList, setMessageList] = useState([]);
 
-    const sendMessage = (author,text) =>{
+    const sendMessage = (author, text) => {
         const newMessageList = [...messageList];
         const newMessage = {
             author,
             text,
-            id: Date.now(),        
+            id: Date.now(),
         };
         newMessageList.push(newMessage);
         setMessageList(newMessageList);
     };
 
     const onSendMessage = (value) => {
-        sendMessage ("user",value);
+        sendMessage("user", value);
     };
 
     useEffect(() => {
-        if(messageList.length ===0) {
+        if (messageList.length === 0) {
             return;
         }
-        const tail = messageList[messageList.length -1];
-        if(tail.author === "bot"){
+        const tail = messageList[messageList.length - 1];
+        if (tail.author === "bot") {
             return;
         }
-        sendMessage("bot","Welcome");
-    },[messageList]);
+        sendMessage("bot", "Welcome");
+    }, [messageList]);
     return (
         <div className={classes.wrapper}>
             <ChatList
-            list={[
-                {
-                    name: "name",
-                    id: "1"
-                },
-                {
-                    name: "name",
-                    id: "2"
-                },
-                {
-                    name: "name",
-                    id:"3"
-                },
-                {
-                    name:"name",
-                    id:"4"
-                }
+                list={[
+                    {
+                        name: "name",
+                        id: "1"
+                    },
+                    {
+                        name: "name",
+                        id: "2"
+                    },
+                    {
+                        name: "name",
+                        id: "3"
+                    },
+                    {
+                        name: "name",
+                        id: "4"
+                    }
 
-            ]}
+                ]}
             />
             <div>
-                <MessageList messageList={messageList}/>
+                <MessageList messageList={messageList} />
                 <MessageInput onSend={onSendMessage} />
             </div>
         </div>
-        
+
     );
- };
+};
 
 
-    
