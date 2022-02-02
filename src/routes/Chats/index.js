42ae1e4f67cbd39855bed05d79f7f593ad7e1b3a
Yuriy <xstream_06@mail.ru>
2022-02-02 23:04:38 +0300

    Lesson6HW

--- src/routes/Chats/index.js
+++ src/routes/Chats/index.js
@@ -1,8 +1,16 @@
 import { Route, Switch } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { ChatList } from "../../components/ChatList";
-
+import { useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from '@material-ui/core';
+import { Messages } from "../Messages";
+import { getChatList } from "../../store/chats/selectors.js";
+import { createChat, removeChat, setChats } from "../../store/chats/actions";
+import { nanoid } from "nanoid";
 import { CHATS } from "../../mocks/chats";
+import { removeMessagesByChatID } from "../../store/messages/actions";
+
 
 const useStyles = makeStyles({
     wrapper: {
@@ -13,11 +21,34 @@ const useStyles = makeStyles({
 
 export const Chats = () => {
     const classes = useStyles();
+    const chats = useSelector(getChatList);
+    const dispatch = useDispatch();
+
+    const onCreate = useCallback(() => {
+        dispatch(createChat({
+            id: nanoid(),
+            name: 'chatName'
+        }))
+    }, []);
+    const onDelete = (chatId) => {
+        dispatch(removeChat(chatId))
+        dispatch(removeMessagesByChatID(chatId))
+    }
+    useEffect(() => {
+        dispatch(setChats(CHATS))
+    }, [])
     return (
 
         <div className={classes.wrapper}>
-            <ChatList list={CHATS} />
-
+            <div>
+                <ChatList onDelete={onDelete} list={chats} />
+                <Button onClick={onCreate}> Create chat</Button>
+            </div>
+            <div>
+                <Switch>
+                    <Route component={Messages} path="/chats/:chatId" />
+                </Switch>
+            </div>
         </div>
     );
 };
