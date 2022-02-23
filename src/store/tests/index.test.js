b5e89ae0ed1dbf31fa44311e2a15634afd555c22
Yuriy <xstream_06@mail.ru>
2022-02-23 15:55:15 +0300

    Lesson10HW

---
+++ src/store/tests/index.test.js
@@ -0,0 +1,26 @@
+import { initialState, chatsReducer } from "../chats/reducer";
+import { createChat } from "../../../entities/";
+import { createAddChat } from "../actions";
+
+
+
+describe('chatsReducer', () => {
+
+    it('вызов редьюсера без экшена вернет initialState', () => {
+        const result = playersReducer();
+
+        expect(result).toEqual(initialState);
+    });
+
+    it('чат добавляется в конец списка', () => {
+        const chat = createChat('chat-1');
+        const result = chatsReducer(undefined, createAddChat(chat));
+
+        expect(result).toEqual({
+            chats: [
+                chat
+            ]
+        });
+    });
+
+})
